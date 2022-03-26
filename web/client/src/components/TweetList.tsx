27ba462fc747bb46705910dd0add4f73563f08f3
jc <jacosta291@gmail.com>
2022-03-26 14:11:49 -0600

    Add JSON.stringify wrap to process.env on client

--- web/client/src/components/TweetList.tsx
+++ web/client/src/components/TweetList.tsx
@@ -10,12 +10,12 @@ import TweetRow from './TweetRow';
 dotenv.config({ path: '../../.env' });
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY as string,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN as string,
-  projectId: process.env.REACT_APP_PROJECT_ID as string,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET as string,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID as string,
-  appId: process.env.REACT_APP_APP_ID as string,
+  apiKey: JSON.stringify(process.env.REACT_APP_API_KEY),
+  authDomain: JSON.stringify(process.env.REACT_APP_AUTH_DOMAIN),
+  projectId: JSON.stringify(process.env.REACT_APP_PROJECT_ID),
+  storageBucket: JSON.stringify(process.env.REACT_APP_STORAGE_BUCKET),
+  messagingSenderId: JSON.stringify(process.env.REACT_APP_MESSAGING_SENDER_ID),
+  appId: JSON.stringify(process.env.REACT_APP_APP_ID),
 };
 
 const app = initializeApp(firebaseConfig);
