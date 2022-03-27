39290962501324bd812dd567dbc126c7caab31d8
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2022-03-27 13:34:12 -0600

    Refact app's structure and scripts

--- server/config/config.ts
+++ server/config/config.ts
@@ -2,17 +2,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const SERVER_PORT = process.env.SERVER_PORT || 3001;
-const SERVER_HOST = process.env.SERVER_HOST || 'localhost';
+const PORT = process.env.PORT || 3001;
 
-const API_URL = process.env.API_URL || 'localhost:5000/';
-
-const SERVER = {
-  port: SERVER_PORT,
-  host: SERVER_HOST,
-  api: {
-    url: API_URL,
-  },
+export const SERVER = {
+  port: PORT,
 };
 
-export default SERVER;
+const TWITTER_APP_API_KEY = process.env.TWITTER_APP_API_KEY || '';
+const TWITTER_APP_API_SECRET = process.env.TWITTER_APP_API_SECRET || '';
+const TWITTER_ACCESS_TOKEN_KEY = process.env.TWITTER_ACCESS_TOKEN_KEY || '';
+const TWITTER_ACCESS_TOKEN_SECRET =
+  process.env.TWITTER_ACCESS_TOKEN_SECRET || '';
+
+export const TWITTER = {
+  consumer_key: TWITTER_APP_API_KEY,
+  consumer_secret: TWITTER_APP_API_SECRET,
+  access_token_key: TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
+};
