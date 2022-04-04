580e33048e14fe2b62ac0226643ba6d1fc61c639
jc <jacosta291@gmail.com>
2022-04-04 08:24:49 -0500

    Add colors and percentages to pie chart, add chart legend, change page layout

--- client/src/components/PieChart.tsx
+++ client/src/components/PieChart.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {VictoryPie} from "victory";
+import {VictoryLegend, VictoryPie} from "victory";
 import {TweetProps} from "./TweetRow";
 
 interface PieChartProps {
@@ -8,26 +8,37 @@ interface PieChartProps {
 export function PieChart(props: PieChartProps) {
 
     let resultsDict : {[sentiment: string] : number} = {}
+    let categoryColors: {[sentiment: string]: string} = {'Negative': '#f73d34', 'Neutral': "Black", 'Positive': "#4ed965"}
     if(props.tweets) {
         props.tweets.map((tweet: TweetProps) => {
-            if (!(tweet.sentiment in resultsDict)) {
                 tweet.sentiment === 0
-                    ? resultsDict['Neutral'] = 0
+                    ? 'Neutral' in resultsDict ? resultsDict['Neutral'] += 1 : resultsDict['Neutral'] = 1
                     : tweet.sentiment > 0
-                        ? resultsDict['Positive'] = 0
-                        : resultsDict['Negative'] = 0
-
-            }
-            tweet.sentiment === 0
-                ? resultsDict['Neutral'] += 1
-                : tweet.sentiment > 0
-                    ? resultsDict['Positive'] += 1
-                    : resultsDict['Negative'] += 1
+                        ? 'Positive' in resultsDict ?  resultsDict['Positive'] += 1 :  resultsDict['Positive'] = 1
+                        : 'Negative' in resultsDict ? resultsDict['Negative'] += 1 : resultsDict['Negative'] = 1
         })
+
     }
     const pieData = Object.entries(resultsDict).map(([key, value]) => {
-        return {x: key, y: value};
+        var total;
+        props.tweets?.length ? total = props.tweets.length : total = 1;
+        let label_val = (value / total) * 100 + "%";
+        return {x: key, y: value, label: label_val};
     })
-    return <VictoryPie data={pieData}/>;
+
+    const legendData = Object.entries(categoryColors).map(([key, value]) => {
+
+        return {name: key, symbol: {fill: value}}
+    })
+
+    return (
+        <>
+
+            <VictoryPie data={pieData} style={{
+            data: {fill: ({datum}) => categoryColors[datum.x]}
+        }}/>
+            <VictoryLegend orientation="horizontal" data={legendData}/>
+        </>
+    );
 }
 
--- client/src/components/TweetRow.tsx
+++ client/src/components/TweetRow.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import exp from "constants";
 
 const rowStyle = {
   border: '1px solid black',
