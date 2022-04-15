b85f1f53a2a2b835a90624ef95105b19ac620ae9
Sebastián Rodríguez Galarza <sebastian_rodriguezg@yahoo.com>
2022-04-15 18:39:54 -0500

    Refactor packages & Firebase config (#12)
    * Add dependencies
    
    * Change app's dependencies structure
    
    * Change sentiment analysis to the server

--- server/routers/tweets.ts
+++ server/routers/tweets.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Router } from 'express';
-import Twitter from 'twitter';
+import Twitter, { ResponseData } from 'twitter';
 import dotenv from 'dotenv';
+import Sentiment from 'sentiment';
 
 import { TWITTER } from '../config/config';
 
+const sentiment = new Sentiment();
 const router = Router();
 
 var twitterClient = new Twitter(TWITTER);
@@ -12,10 +14,15 @@ router.get('/search', async (req: Request, res: Response) => {
   twitterClient
     .get('search/tweets', {
       q: 'coding',
-      count: 10,
+      count: 2,
     })
     .then((data) => {
-      return res.status(200).json(data);
+      const tweets = data.statuses.map((tweet: ResponseData) => ({
+        // TODO: Check which properties to include in the object for the report
+        ...tweet,
+        sentiment: sentiment.analyze(tweet.text).score,
+      }));
+      return res.status(200).json(tweets);
     })
     .catch((err) => {
       return res.status(500).json({ message: err.message, err });
