8cb298fb7b2ff2b9f895d5cda7c63c165f05b22a
KevinTMtz <torresmtzkevin@gmail.com>
2022-05-14 22:26:40 -0500

    Dynamically render search report items

--- client/src/components/report/Report.tsx
+++ client/src/components/report/Report.tsx
@@ -7,28 +7,70 @@ import {
   upperCaseFirstLetter,
 } from '../../functions/utils/utils';
 import Map from './Map';
+import { objectWithKeyStr } from '../../types/types';
 
 interface ReportProps {
   report: any;
 }
 
-const Report = (props: ReportProps) => (
-  <Grid>
-    <Typography variant='h4' textAlign='center'>
-      {upperCaseFirstLetter(props.report.query.topic)}
-      's report:
-    </Typography>
-    <Typography variant='h6' textAlign='center'>
-      Created at: {getDateAndTime(props.report.query.created_at)}
-    </Typography>
-    <Typography variant='h6' textAlign='center'>
-      {props.report.statistics.total} tweets found
-    </Typography>
-    <ReportChart {...props.report.charts.generalSentiment} />
-    <ReportChart {...props.report.charts.accumulatedSentiment} />
-    <Map {...props.report.maps.totalTweets} />
-    <Map {...props.report.maps.averageSentiment} />
-  </Grid>
-);
+interface reportSection extends objectWithKeyStr {
+  charts: string[];
+  maps: string[];
+}
+
+interface reportSections extends objectWithKeyStr {
+  sentiment: reportSection;
+  statistics: reportSection;
+}
+
+const Report = (props: ReportProps) => {
+  const reportContent: reportSections = {
+    sentiment: {
+      charts: ['generalSentiment', 'accumulatedSentiment'],
+      maps: ['totalTweets'],
+    },
+    statistics: { charts: [], maps: ['averageSentiment'] },
+  };
+
+  const getElement = (elementType: string, key: string, elementProps: any) => {
+    switch (elementType) {
+      case 'charts':
+        return <ReportChart {...elementProps} key={key} />;
+
+      case 'maps':
+        return <Map {...elementProps} key={key} />;
+    }
+  };
+
+  return (
+    <Grid>
+      <Typography variant='h4' textAlign='center'>
+        {upperCaseFirstLetter(props.report.query.topic)}
+        's report:
+      </Typography>
+      <Typography variant='h6' textAlign='center'>
+        Created at: {getDateAndTime(props.report.query.created_at)}
+      </Typography>
+      <Typography variant='h6' textAlign='center'>
+        {props.report.statistics.total} tweets found
+      </Typography>
+
+      {Object.keys(reportContent).map((section: string) =>
+        Object.keys(reportContent[section]).map((elementType: string) =>
+          reportContent[section][elementType].map((elementName: string) =>
+            props.report.hasOwnProperty(elementType) &&
+            props.report[elementType].hasOwnProperty(elementName)
+              ? getElement(
+                  elementType,
+                  elementName,
+                  props.report[elementType][elementName],
+                )
+              : null,
+          ),
+        ),
+      )}
+    </Grid>
+  );
+};
 
 export default Report;
