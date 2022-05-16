8717a6e17e17878e6750e2daeac505003b129116
KevinTMtz <torresmtzkevin@gmail.com>
2022-05-16 12:16:15 -0500

    Fade in landing page elements

--- client/src/containers/LandingPage.tsx
+++ client/src/containers/LandingPage.tsx
@@ -6,13 +6,22 @@ import Header from '../components/landing-page/sections/Header';
 import Pricing from '../components/landing-page/sections/Pricing';
 import Services from '../components/landing-page/sections/Services';
 import Footer from '../components/landing-page/sections/Footer';
+import FadeInSection from '../components/utils/FadeInSection';
 
 const LandingPage = () => (
   <Box>
-    <Header />
-    <Services />
-    <Pricing />
-    <Contact />
+    <FadeInSection>
+      <Header />
+    </FadeInSection>
+    <FadeInSection>
+      <Services />
+    </FadeInSection>
+    <FadeInSection>
+      <Pricing />
+    </FadeInSection>
+    <FadeInSection>
+      <Contact />
+    </FadeInSection>
     <Footer />
   </Box>
 );
